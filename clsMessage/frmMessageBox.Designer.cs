139298b17d19ce9de00d39ecb2b97238543e25cc
Abdulghani99I <abooooody6666633321@gmail.com>
2024-11-08 17:08:03 +0300

    first commit

---
+++ clsMessage/frmMessageBox.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace clsMessageBox
+{
+    partial class frmMessageBoxDev
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.pictureEditIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEditIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn1.Location = new System.Drawing.Point(206, 147);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(91, 40);
+            this.btn1.TabIndex = 0;
+            this.btn1.TabStop = false;
+            this.btn1.Text = "Button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click_1);
+            // 
+            // btn2
+            // 
+            this.btn2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn2.Location = new System.Drawing.Point(64, 147);
+            this.btn2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(91, 40);
+            this.btn2.TabIndex = 1;
+            this.btn2.TabStop = false;
+            this.btn2.Text = "Button2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click_1);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblMessage.Location = new System.Drawing.Point(20, 54);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(336, 68);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "Show Message";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureEditIcon
+            // 
+            this.pictureEditIcon.Location = new System.Drawing.Point(12, -4);
+            this.pictureEditIcon.Name = "pictureEditIcon";
+            this.pictureEditIcon.Size = new System.Drawing.Size(51, 66);
+            this.pictureEditIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureEditIcon.TabIndex = 7;
+            this.pictureEditIcon.TabStop = false;
+            // 
+            // frmMessageBoxDev
+            // 
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 202);
+            this.Controls.Add(this.pictureEditIcon);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.lblMessage);
+            this.Font = new System.Drawing.Font("Cairo", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMessageBoxDev";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MessageBox";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEditIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.PictureBox pictureEditIcon;
+    }
+}
---
+++ clsMessage/frmMessageBox.cs
@@ -0,0 +1,168 @@
+﻿using DevExpress.Security;
+using DevExpress.Utils.CommonDialogs.Internal;
+using DevExpress.XtraEditors;
+using DevExpress.XtraExport.Xls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpressInternal = DevExpress.Utils.CommonDialogs.Internal;
+
+namespace clsMessageBox
+{
+    public partial class frmMessageBoxDev : DevExpress.XtraEditors.XtraForm
+    {
+        static private DevExpressInternal.DialogResult _DialogResult;
+
+
+        public enum ModeDialog { Question, Information, Error, None}
+        ModeDialog mode = ModeDialog.None;
+        public enum Focus { btn1, btn2, none }
+
+        Focus focus = Focus.none;
+
+        public void PlaySound(byte[] NameFile)
+        {
+            // استخدام MemoryStream لقراءة بيانات الصوت من المصفوفة
+            using (MemoryStream memoryStream = new MemoryStream(NameFile))
+            {
+                // إنشاء كائن SoundPlayer وتشغيل الصوت
+                using (SoundPlayer player = new SoundPlayer(memoryStream))
+                {
+                    player.Play();
+                }
+            }
+        }
+
+        void RunMode(ModeDialog Mode)
+        {
+            switch (Mode)
+            {
+                case ModeDialog.Question:
+                    pictureEditIcon.Image = Properties.Resources.question_32x32;
+                    PlaySound(Properties.Resources.Question);
+                    break;
+
+                case ModeDialog.Error:
+                    pictureEditIcon.Image = Properties.Resources.deletelist_32x32;
+                    PlaySound(Properties.Resources.Error);
+                    break;
+
+                case ModeDialog.Information:
+                    pictureEditIcon.Image = Properties.Resources.about_32x32;
+                    PlaySound(Properties.Resources.Information);
+                    break;
+
+
+                case ModeDialog.None:
+                default:
+                    //pictureEditIcon.Image = Properties.Resources.deletelist_32x32;
+                    //PlaySound(Properties.Resources.Error);
+                    break;
+            }
+        }
+
+        void SetFocusButton(Focus focus)
+        {
+            // set focus buttons.
+            switch (focus)
+            {
+                case Focus.btn1:
+                    btn1.TabStop = true;
+                    //btn1.Focus();
+                    break;
+                case Focus.btn2:
+                    btn2.TabStop = true;
+                    //btn2.Focus();
+                    break;
+
+                default:
+                    this.Focus(); // Set the focus to the form itself, canceling focus on other controls.
+                    break;
+            }
+        }
+
+        frmMessageBoxDev(string message, string title, string button1Text, string button2Text, ModeDialog Mode, Focus focus = Focus.none)
+        {
+            InitializeComponent();
+            
+            RunMode(Mode);
+
+            _DialogResult = DevExpressInternal.DialogResult.None; // This line is important because is static and public.
+
+
+            lblMessage.Text = message;
+            this.Text = title;
+
+            btn1.Text = button1Text;
+
+            if (string.IsNullOrEmpty(button2Text))
+            {
+                // If there's only one button, hide the second one
+                btn2.Visible = false;
+                btn1.Left = (this.ClientSize.Width - btn1.Width) / 2; // Center the button
+            }
+            else
+            {
+                btn2.Text = button2Text;
+            }
+
+            SetFocusButton(focus);
+        }
+
+        frmMessageBoxDev(string message, string title, string button1Text, ModeDialog Mode, Focus focus = Focus.none)
+        {
+            InitializeComponent();
+
+            RunMode(Mode);
+
+
+            _DialogResult = DevExpressInternal.DialogResult.None; // This line is important because is static and public
+
+            lblMessage.Text = message;
+            this.Text = title;
+
+            btn1.Text = button1Text;
+
+            btn1.Left = (this.ClientSize.Width - btn1.Width) / 2; // Center the button
+            btn2.Visible = false; // No Threre btn2 In this Constractor.
+
+            SetFocusButton(focus);
+        }
+
+        public static DevExpressInternal.DialogResult ShowDialog(string message, string title, string button1Text, string button2Text, ModeDialog Mode, Focus focus = Focus.none)
+        {
+            frmMessageBoxDev msgBox = new frmMessageBoxDev(message, title, button1Text, button2Text, Mode, focus);
+            msgBox.ShowDialog();
+            return _DialogResult;
+        }
+        
+        public static DevExpressInternal.DialogResult ShowDialog(string message, string title, string button1Text, ModeDialog Mode, Focus focus = Focus.none)
+        {
+            frmMessageBoxDev msgBox = new frmMessageBoxDev(message, title, button1Text, Mode, focus);
+            msgBox.ShowDialog();
+            return _DialogResult;
+        }
+
+        private void btn1_Click_1(object sender, EventArgs e)
+        {
+            _DialogResult = DevExpressInternal.DialogResult.Yes; // Default action for the first button
+            this.Close();
+        }
+
+        private void btn2_Click_1(object sender, EventArgs e)
+        {
+            _DialogResult = DevExpressInternal.DialogResult.No; // Default action for the second button
+            this.Close();
+        }
+    }
+}
